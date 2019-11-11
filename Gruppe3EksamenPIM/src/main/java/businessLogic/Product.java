7f1e26bd5429bb8ae1c3fe10d1999ba5aa0cb460
Marcus Johnsen <cph-mj734@cphbusiness.dk>
2019-11-11 10:30:59 +0100

    adding product and finding latest ID

--- Gruppe3EksamenPIM/src/main/java/businessLogic/Product.java
+++ Gruppe3EksamenPIM/src/main/java/businessLogic/Product.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package businessLogic;
 
 import java.util.ArrayList;
@@ -50,9 +45,4 @@ public class Product {
     public static ArrayList<Product> getProductList() {
         return productList;
     }
-    
-    
-    
-    
-    
 }
