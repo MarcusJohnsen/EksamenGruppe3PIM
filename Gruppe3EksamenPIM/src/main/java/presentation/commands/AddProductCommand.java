44c99055f34765e001a41b8496e29142a8d7e9be
cahit <cahit10@hotmail.com>
2019-11-11 10:18:23 +0100

    added command function/files

---
+++ Gruppe3EksamenPIM/src/main/java/presentation/commands/AddProductCommand.java
@@ -0,0 +1,19 @@
+
+package presentation.commands;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import presentation.Command;
+
+/**
+ *
+ * @author cahit
+ */
+public class AddProductCommand extends Command{
+
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
