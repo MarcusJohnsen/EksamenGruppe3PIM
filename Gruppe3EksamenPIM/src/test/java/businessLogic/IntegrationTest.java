f9e86a70a1bad3fa39c2dab8b7ad673f46dbe6f9
cahit <cahit10@hotmail.com>
2019-11-18 16:17:12 +0100

    Tests for validateNewAttribute

--- Gruppe3EksamenPIM/src/test/java/businessLogic/IntegrationTest.java
+++ Gruppe3EksamenPIM/src/test/java/businessLogic/IntegrationTest.java
@@ -11,6 +11,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import org.junit.Test;
 import org.junit.Before;
 import static org.junit.Assert.*;
@@ -24,8 +25,6 @@ public class IntegrationTest {
     private static Connection testConnection;
     private final BusinessFacade businessFacade = new BusinessFacade(SystemMode.TEST);
 
-    ;
-    
     @Before
     public void setup() {
         try {
@@ -35,9 +34,15 @@ public class IntegrationTest {
                 stmt.execute("drop table if exists Categories");
                 stmt.execute("create table Categories like Categories_Test");
                 stmt.execute("insert into Categories select * from Categories_Test");
+                
                 stmt.execute("drop table if exists Product");
                 stmt.execute("create table Product like Product_Test");
                 stmt.execute("insert into Product select * from Product_Test");
+                
+                stmt.execute("drop table if exists Attributes");
+                stmt.execute("create table Attributes like Attributes_Test");
+                stmt.execute("insert into Attributes select * from Attributes_Test");
+                
                 stmt.execute("drop table if exists Product_Distributor");
                 stmt.execute("create table Product_Distributor like Product_Distributor_Test");
                 stmt.execute("insert into Product_Distributor select * from Product_Distributor_Test");
@@ -58,38 +63,38 @@ public class IntegrationTest {
 
     //Category createNewCategory(String categoryName, String categoryDescription) throws IllegalArgumentException
     @Test
-    public void testCreateNewCategory(){
+    public void testCreateNewCategory() {
         //arrange
         String categoryName = "New Category";
         String categoryDescription = "This is a new category, for new things";
-        
+
         //act
         Category result = businessFacade.createNewCategory(categoryName, categoryDescription);
-        
+
         //assert
         assertTrue(categoryName.equals(result.getName()));
         assertTrue(categoryDescription.equals(result.getDescription()));
         assertTrue(businessFacade.getCategoryList().contains(result));
     }
-    
+
     //boolean deleteCategory(int categoryID)
     @Test
-    public void testDeleteCategory(){
+    public void testDeleteCategory() {
         //arrange
         int categoryID = 1;
-        
+
         //act
         boolean result = businessFacade.deleteCategory(categoryID);
-        
+
         //assert
         ArrayList<Category> categoryList = businessFacade.getCategoryList();
         assertTrue(result);
         for (Category category : categoryList) {
             assertNotEquals(categoryID, category.getCategoryID());
         }
-        
+
     }
-    
+
     //Product createNewProduct(String productName, String productDescription, ArrayList<String> productDistributors)
     @Test
     public void testCreateNewProduct() {
@@ -109,16 +114,16 @@ public class IntegrationTest {
         assertEquals(productDistributors, result.getDistributors());
         assertTrue(businessFacade.getProductList().contains(result));
     }
-    
+
     //boolean deleteProduct(int productID)
     @Test
-    public void testDeleteProduct(){
+    public void testDeleteProduct() {
         //arrange
         int productID = 1;
-        
+
         //act
         boolean result = businessFacade.deleteProduct(productID);
-        
+
         //assert
         ArrayList<Product> productList = businessFacade.getProductList();
         assertTrue(result);
@@ -126,46 +131,46 @@ public class IntegrationTest {
             assertNotEquals(productID, product.getProductID());
         }
     }
-    
+
     //void editProduct(int productID, String productName, String productDescription, ArrayList<String> productDistributors)
     @Test
-    public void testEditProduct(){
+    public void testEditProduct() {
         //arrange
         int productID = 1;
         String productName = "Newest Product";
         String productDescription = "This is new newest product for testing";
         String firstDistributor = "1st Distributor";
         String secondDistributor = "2nd Distributor";
-        ArrayList<String> productDistributors = new ArrayList(Arrays.asList(new String[] {firstDistributor, secondDistributor}));
-        
+        ArrayList<String> productDistributors = new ArrayList(Arrays.asList(new String[]{firstDistributor, secondDistributor}));
+
         //act
         businessFacade.editProduct(productID, productName, productDescription, productDistributors);
         Product result = businessFacade.getProductFromID(productID);
-        
+
         //assert
         assertTrue(result.getName().equals(productName));
         assertTrue(result.getDescription().equals(productDescription));
         assertTrue(result.getDistributors().contains(firstDistributor));
         assertTrue(result.getDistributors().contains(secondDistributor));
-        
+
     }
-    
+
     //void updatePicturePath(int productID, String picturePath)
     @Test
-    public void testUpdatePicturePath(){
+    public void testUpdatePicturePath() {
         //arrange
         int productID = 1;
         String picturePath = "newPic.img";
-        
+
         //act
         businessFacade.updatePicturePath(productID, picturePath);
         Product result = businessFacade.getProductFromID(productID);
-        
+
         //assert
         assertTrue(picturePath.equals(result.getPicturePath()));
-        
+
     }
-    
+
     @Test
     public void testGetCategoryFromID() {
         //arrange
@@ -174,12 +179,53 @@ public class IntegrationTest {
         String categoryDist = "For landscaping in garden";
         Category category = new Category(categoryID, categoryName, categoryDist);
         Category.addToCategoryList(category);
-        
+
         //act
         Category result = businessFacade.getCategoryFromID(categoryID);
-        
+
         //assert
         assertTrue(categoryName.equals(result.getName()));
         assertTrue(categoryDist.equals(result.getDescription()));
     }
-}
+
+    @Test
+
+    public void testCreateNewAttribute() {
+
+        String attributeTitle1 = "tester1";
+        String attributeTitle2 = "tester2";
+        String attributeTitle3 = "tester3";
+
+        Attribute result1 = businessFacade.createNewAttribute(attributeTitle1);
+        Attribute result2 = businessFacade.createNewAttribute(attributeTitle2);
+        Attribute result3 = businessFacade.createNewAttribute(attributeTitle3);
+
+        int expIDResult1 = 6;
+        int expIDResult2 = expIDResult1 + 1;
+        int expIDResult3 = expIDResult2 + 1;
+        
+        assertEquals(expIDResult1,result1.getAttributeID());
+        assertEquals(expIDResult2,result2.getAttributeID());
+        assertEquals(expIDResult3,result3.getAttributeID());
+        
+        assertTrue(attributeTitle1.equals(result1.getAttributeTitle()));
+        assertTrue(attributeTitle2.equals(result2.getAttributeTitle()));
+        assertTrue(attributeTitle3.equals(result3.getAttributeTitle()));
+
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNegativeValidateNewAttributeTitleNoAttributeTitle(){
+        
+        String attributeTitle1 = "";
+        
+        Attribute result1 = businessFacade.createNewAttribute(attributeTitle1);
+        
+        
+        
+    }
+    
+    
+    
+    }
